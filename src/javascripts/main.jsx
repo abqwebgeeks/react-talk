8c5cb34dc75f30ae43d2f7ed255ace4b8307e764
Damon Toal-Rossi <damon@uber.com>
2016-02-29 16:29:05 -0700

    Add React hot loading

--- src/javascripts/main.jsx
+++ src/javascripts/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const HelloWorld = () => (
-  <h1>Hello World</h1>
+  <h1>Hello World!</h1>
 );
 
 ReactDOM.render(<HelloWorld />, document.getElementById('app-container'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 var path = require('path');
 var webpack = require('webpack');
 
+var jsxTest = /\.jsx?/;
+var jsxPath = path.join(__dirname, 'src/javascripts');
+
 module.exports = {
-  entry: './src/javascripts/main.jsx',
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:8080',
+    'webpack/hot/only-dev-server',
+    './src/javascripts/main.jsx'
+  ],
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'main.js'
@@ -10,9 +17,14 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /.jsx?$/,
+        test: jsxTest,
+        loaders: ['react-hot'],
+        include: jsxPath
+      },
+      {
+        test: jsxTest,
         loader: 'babel-loader',
-        exclude: /node_modules/,
+        include: jsxPath,
         query: {
           presets: ['es2015', 'react']
         }
