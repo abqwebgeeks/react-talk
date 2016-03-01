f9cdd899041d27bbd2249456e4615c5d49d8fcb0
Damon Toal-Rossi <damon@uber.com>
2016-02-29 18:05:33 -0700

    Move app to container for clarity

--- src/javascripts/main.jsx
+++ src/javascripts/main.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import '../stylesheets/main.scss'
 
-import App from './components/app.jsx';
+import App from './containers/app.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app-container'));
