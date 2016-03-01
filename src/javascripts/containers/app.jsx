a67f5342f8b57b010bbde99750b7e12793d157a0
Damon Toal-Rossi <damon@uber.com>
2016-02-29 18:33:30 -0700

    Refactor todo list out of render() function

--- src/javascripts/containers/app.jsx
+++ src/javascripts/containers/app.jsx
@@ -2,27 +2,27 @@ import React, {Component} from 'react';
 
 import Todos from '../components/todos.jsx';
 
+const todos = [
+  {
+    complete: true,
+    text: 'Add an input for adding todos'
+  },
+  {
+    complete: false,
+    text: 'Add a few dummy todos'
+  },
+  {
+    complete: false,
+    text: 'Implement ability to add todos'
+  },
+  {
+    complete: false,
+    text: 'Implement ability to complete todos'
+  }
+];
+
 class App extends Component {
   render() {
-    const todos = [
-      {
-        complete: true,
-        text: 'Add an input for adding todos'
-      },
-      {
-        complete: false,
-        text: 'Add a few dummy todos'
-      },
-      {
-        complete: false,
-        text: 'Implement ability to add todos'
-      },
-      {
-        complete: false,
-        text: 'Implement ability to complete todos'
-      }
-    ];
-
     return (
       <div className="todoapp">
         <header className="header">
