38a72ce0411446e4eb5baccf1a751364f1e598e2
Damon Toal-Rossi <damon@uber.com>
2016-02-29 17:45:18 -0700

    Refactor for basic TodoMVC boilerplate

--- src/javascripts/main.jsx
+++ src/javascripts/main.jsx
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 import '../stylesheets/main.scss'
 
 const HelloWorld = () => (
-  <h1>Hello World!!</h1>
+  <div className="todoapp">
+    <h1>Hello World!!</h1>
+  </div>
 );
 
-ReactDOM.render(<HelloWorld />, document.getElementById('todo-app'));
+ReactDOM.render(<HelloWorld />, document.getElementById('app-container'));
