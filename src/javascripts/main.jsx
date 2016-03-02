86ab6d81da137aea317d20ca1fcd6aeba18efe07
Damon Toal-Rossi <damon@uber.com>
2016-03-02 16:04:49 -0700

    use react component for clarity

--- src/javascripts/main.jsx
+++ src/javascripts/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import '../stylesheets/main.scss'
+import '../stylesheets/main.scss';
 
-const HelloWorld = () => (
-  <div className="todoapp">
-    <h1>Hello World!!</h1>
-  </div>
-);
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div className="todoapp">
+        <h1>Hello World!!</h1>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<HelloWorld />, document.getElementById('app-container'));
